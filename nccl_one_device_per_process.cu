3bd3a5c8b6e69a61c7d1991cba48a28bfce2c491
Yuduo Wu <wyd855@gmail.com>
2017-11-13 22:17:41 -0800

    Fix format

--- nccl_one_device_per_process.cu
+++ nccl_one_device_per_process.cu
@@ -6,31 +6,31 @@
 #include <stdint.h>
 #include <iostream>
 
-#define MPICHECK(cmd) do {			            \
-    int e = cmd;				                \
-    if( e != MPI_SUCCESS ) {			        \
-      printf("Failed: MPI error %s:%d '%d'\n",	\
-	     __FILE__,__LINE__, e);		            \
-      exit(EXIT_FAILURE);			            \
+#define MPICHECK(cmd) do {					    \
+    int e = cmd;						    \
+    if( e != MPI_SUCCESS ) {					    \
+      printf("Failed: MPI error %s:%d '%d'\n",			    \
+	     __FILE__,__LINE__, e);				    \
+      exit(EXIT_FAILURE);					    \
     }						                    \
   } while(0)
 
-#define CUDACHECK(cmd) do {				            \
-    cudaError_t e = cmd;				            \
-    if( e != cudaSuccess ) {				        \
-      printf("Failed: Cuda error %s:%d '%s'\n",		\
-	     __FILE__,__LINE__,cudaGetErrorString(e));	\
-      exit(EXIT_FAILURE);				            \
-    }							                    \
+#define CUDACHECK(cmd) do {						\
+    cudaError_t e = cmd;						\
+    if( e != cudaSuccess ) {						\
+      printf("Failed: Cuda error %s:%d '%s'\n",				\
+	     __FILE__,__LINE__,cudaGetErrorString(e));			\
+      exit(EXIT_FAILURE);						\
+    }									\
   } while(0)
 
 #define NCCLCHECK(cmd) do {				            \
     ncclResult_t r = cmd;				            \
     if (r!= ncclSuccess) {				            \
-      printf("Failed, NCCL error %s:%d '%s'\n",		\
-	     __FILE__,__LINE__,ncclGetErrorString(r));	\
+      printf("Failed, NCCL error %s:%d '%s'\n",			    \
+	     __FILE__,__LINE__,ncclGetErrorString(r));		    \
       exit(EXIT_FAILURE);				            \
-    }							                    \
+    }								    \
   } while(0)
 
 static uint64_t getHostHash(const char* string) {
