b3dd84c982f4400cd13ddb55c68aa0dcc3908d15
Yuduo Wu <wyd855@gmail.com>
2018-05-25 11:20:51 -0700

    Refactor

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 mkdir build
 cd build
-cmake3 -DNCCL_LIBRARY=/usr/local/nccl_2.1.15-1+cuda9.1_x86_64/lib/libnccl.so -DNCCL_INCLUDE_DIR=/usr/local/nccl_2.1.15-1+cuda9.1_x86_64/include/ ..
+cmake3 -DNCCL_LIBRARY=/home/yuduo/tools/nccl-2.1.2/lib/libnccl.so -DNCCL_INCLUDE_DIR=/home/yuduo/tools/nccl-2.1.2/include/ ..
+make -j
--- run.sh
+++ run.sh
@@ -2,14 +2,13 @@
 
 MPI_EXE=mpirun
 
-
-$MPI_EXE -np 2 -host 192.168.1.17,192.168.1.16 \
-    -allow-run-as-root -mca btl_openib_warn_default_gid_prefix 0 \
-    -mca btl_openib_want_cuda_gdr 1 --mca btl_tcp_if_include ib0 \
+$MPI_EXE -np 2 -host 192.168.56.133,192.168.56.134 \
+    -mca btl_openib_warn_default_gid_prefix 0 \
+    -mca btl_openib_want_cuda_gdr 1 \
+    --mca btl_tcp_if_include ib0 \
     build/nccl-two-communicators-diff-types-with-threads
 
 exit
-
 
 $MPI_EXE -np 2 -host 192.168.1.17,192.168.1.16 \
     -allow-run-as-root -mca btl_openib_warn_default_gid_prefix 0 \
